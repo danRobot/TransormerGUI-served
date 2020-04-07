89740a529461875cd52699c4b8f1447eb79cd227
danRobot <danielgallo502@gmail.com>
2020-04-06 23:01:59 -0500

    lagre model added

--- transgenerator/train.py
+++ transgenerator/train.py
@@ -75,7 +75,7 @@ logger = logging.getLogger(__name__)
 
 MODEL_CLASSES = {
     "gpt2": (GPT2Config, GPT2LMHeadModel, GPT2Tokenizer),
-    "gpt2-xl": (GPT2Config, GPT2LMHeadModel, GPT2Tokenizer),
+    "gpt2-large": (GPT2Config, GPT2LMHeadModel, GPT2Tokenizer),
     "openai-gpt": (OpenAIGPTConfig, OpenAIGPTLMHeadModel, OpenAIGPTTokenizer),
     "bert": (BertConfig, BertForMaskedLM, BertTokenizer),
     "roberta": (RobertaConfig, RobertaForMaskedLM, RobertaTokenizer),
